766217029eb4f27f4ded10a49b8a4768990d91fc
cutefcc <cutefcc@126.com>
2022-10-16 23:15:59 +0800

    feat: home page

--- src/web/components/Header/index.tsx
+++ src/web/components/Header/index.tsx
@@ -1,34 +1,32 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  MenuRouteConfig,
-  MenuRouteConfigType,
-} from "@constants/MenuRouteConfig";
-import {
-  TopHeader,
-  Slogan,
-  RightBtn,
-  walletButtonStyle,
-  Content,
-} from "./style";
-import {
-  NetworkType,
-  BeaconEvent,
-  defaultEventCallbacks,
-} from "@airgap/beacon-sdk";
-import ConnectWallet from "@components/ConnectWallet";
-import { getCurretnRoute } from "@utils/getCurrentRoute";
-import { useAtom } from "jotai";
-import { store } from "@store/jotaiStore";
-import { StoreType } from "@type/index";
-import { useImmer } from "@hooks/useImmer";
+import { useNavigate } from 'react-router-dom';
+import { TopHeader } from './style';
+import Button from '@mui/material/Button';
+
+const ButtonStyle = {
+  backgroundColor: '#00AC55',
+};
 
 const Header = () => {
   const navigate = useNavigate();
 
+  const handleContactUs = () => {};
+
   return (
-    <TopHeader>
-      <Content></Content>
+    <TopHeader className="h-114">
+      <div className="w-1280  h-full flex items-center m-auto">
+        <div className="w-1280 h-full flex items-center ">
+          <img className="mr-15" src="public/logo.png" />
+          <div className="text-[36px] text-[#fff]">Cambio Network</div>
+        </div>
+        <Button
+          onClick={handleContactUs}
+          className="w-184 h-54"
+          variant="contained"
+          style={ButtonStyle}
+        >
+          <div className="normal-case text-[24px]">Contact Us</div>
+        </Button>
+      </div>
     </TopHeader>
   );
 };
--- src/web/pages/Home/index.tsx
+++ src/web/pages/Home/index.tsx
@@ -1,32 +1,26 @@
-import { memo, useEffect } from "react";
-import Button from "@mui/material/Button";
-import {
-  Container,
-  Content,
-  Slogan,
-  SloganDesc,
-  getStartBtn,
-  DownLoadDeck,
-} from "./style";
-import { useNavigate } from "react-router-dom";
-import { MenuRouteConfig } from "@constants/MenuRouteConfig";
-import Header from "@components/Header";
+import { memo, useEffect } from 'react';
+import { Container } from './style';
+import Header from '@components/Header';
 
-function Home() {
-  // const navigate = useNavigate();
+const Home = () => {
   useEffect(() => {}, []);
-  // const handleGoDaoLists = () => {
-  //   navigate(MenuRouteConfig?.daoLists?.route);
-  // };
   return (
     <>
       <Header />
-      <Container className="flex flex-col justify-center items-center w-full">
-        <Content className="w-full h-292 flex flex-col justify-between items-center">
-          Cambio
-        </Content>
-      </Container>
+      <div className="w-full bg-[#1a2239]">
+        <Container className="flex h-620 justify-between items-center w-1280 m-auto text-[#1a2239]">
+          <div className="w-586">
+            <div className="w-586 xl:text-[3.75rem] leading-[1.2] text-[#fff]">
+              The DNS <span className="text-[#03AB55]">BRIDGE</span> for web2 to web3.
+            </div>
+            <div className="w-586 h-64 xl:text-[1.25rem] text-[#AEB0B4]">
+              Decentralized and Native DNS Service for both Web2 and Web3.
+            </div>
+          </div>
+          <img className="w-586" src="/public/home_big_img.png" />
+        </Container>
+      </div>
     </>
   );
-}
+};
 export default memo(Home);
